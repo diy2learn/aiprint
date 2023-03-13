57bd8fe0409c1c0f90a01bf0eee3ded5ed76cff6
diy2learn <bian.tquang@gmail.com>
2023-03-13 14:04:58 +0100

    add test_add_color

--- tests/unittest/test_helper.py
+++ tests/unittest/test_helper.py
@@ -1,4 +1,4 @@
-from aiprint.helper import extract_status, str2bool
+from aiprint.helper import add_color, extract_status, str2bool
 
 
 def test_str2bool():
@@ -9,3 +9,10 @@ def test_str2bool():
 def test_extract_status():
     data = {"success": False, "other_field": 123}
     assert extract_status(str(data), pattern="'success': (\w+)") == "False"
+
+
+def test_add_color():
+    data = {"success": False, "other_field": 123}
+    data_str = str(data)
+    pattern = "'success': False"
+    assert pattern in add_color(data_str, pattern, status="f")
