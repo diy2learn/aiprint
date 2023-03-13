f721c6fbb28a713ff90f4797a135a32dd9f9ac23
diy2learn <bian.tquang@gmail.com>
2023-03-13 12:47:48 +0100

    add helper module

---
+++ src/aiprint/helper.py
@@ -0,0 +1,31 @@
+"""helper functions"""
+
+
+import re
+
+from termcolor import colored
+
+
+def str2bool(v):
+    return v.lower() in ("yes", "true", "t", "1")
+
+
+def extract_status(data_str: str, pattern: str):
+    """
+
+    Examples
+    --------
+    data_str = {"success": false,
+                "other_field": 123}
+    """
+    extracted = re.findall(pattern, data_str)
+    if len(extracted) == 0:
+        print(colored("Can not find pattern to highlight", "red"))
+    else:
+        return extracted[0]
+
+
+def add_color(data_str: str, pattern: str, status: str):
+    color = "green" if str2bool(status) else "red"
+    colored_txt = colored(pattern, color)
+    return data_str.replace(pattern, colored_txt)
