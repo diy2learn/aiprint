af19c5701fe712121a33574e543fc7fb87e20127
diy2learn <bian.tquang@gmail.com>
2023-03-13 12:58:09 +0100

    add test_extract_status

--- tests/unittest/test_helper.py
+++ tests/unittest/test_helper.py
@@ -1,6 +1,11 @@
-from aiprint.helper import str2bool
+from aiprint.helper import extract_status, str2bool
 
 
 def test_str2bool():
     assert str2bool("yes")
     assert not str2bool("n")
+
+
+def test_extract_status():
+    data = {"success": False, "other_field": 123}
+    assert extract_status(str(data), pattern="'success': (\w+)") == "False"
